bd383940ff221ece8665729f26043dd3118e6975
Mikalai <mb@3nsoft.com>
2026-05-07 18:39:41 -0400

    Version 0.8.13

--- src/common/services/base/random.ts
+++ src/common/services/base/random.ts
@@ -1,5 +1,5 @@
 /*
- Copyright (C) 2022 3NSoft Inc.
+ Copyright (C) 2022, 2026 3NSoft Inc.
 
  This program is free software: you can redistribute it and/or modify it under
  the terms of the GNU General Public License as published by the Free Software
@@ -22,13 +22,28 @@ export function randomStr(numOfChars: number): string {
     throw new Error(`number of chars is less than one`);
   }
   const byteLen = 3 * (Math.floor(numOfChars / 4) + 1);
-  const bytes = new Uint8Array(byteLen);
-  crypto.getRandomValues(bytes);
-  return bytesToUrlSafeBase64(bytes).slice(0, numOfChars);
+  const bytes = randomBytes(byteLen);
+  /* eslint-disable  @typescript-eslint/no-explicit-any */
+  const str = ((bytes as any).toBase64 ?
+    (bytes as any).toBase64({ alphabet: 'base64url' }) :
+    bytesToUrlSafeBase64(bytes)
+  );
+  return str.slice(0, numOfChars);
 }
 
 export function randomBytes(numOfBytes: number): Uint8Array {
-  const bytes = new Uint8Array(numOfBytes);
-  crypto.getRandomValues(bytes);
-  return bytes;
+  if ((globalThis as Partial<typeof globalThis>).crypto?.getRandomValues) {
+    const bytes = new Uint8Array(numOfBytes);
+    crypto.getRandomValues(bytes);
+    return bytes;
+  } else {
+    console.warn(`Using math to get random value, in when crypto is missing.`);
+    const numOfRandU32s = Math.floor(numOfBytes/4) + ((numOfBytes%4 > 0) ? 1 : 0);
+    const u32s = new Uint32Array(numOfRandU32s);
+    for (let i=0; i<u32s.length; i+=1) {
+      u32s[i] = Math.floor(0xffffffff * Math.random());
+    }
+    const u8s = new Uint8Array(u32s.buffer);
+    return ((u8s.length > numOfBytes) ? u8s.slice(0, numOfBytes) : u8s);
+  }
 }
